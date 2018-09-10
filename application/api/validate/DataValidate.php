c535bdf71d663d4c153baa79667658d7e7a05007
shmilylbelva <1028604181@qq.com>
2018-09-10 17:51:46 +0800

    1

--- application/api/validate/DataValidate.php
+++ application/api/validate/DataValidate.php
@@ -1,35 +1,39 @@
 <?php
 /**
  * Created by shmilyelva
- * Date: 2018/9/6
- * Time: 下午10:09
+ * Date: 2018-09-06
+ * Time: 17:07
  */
 
 namespace app\api\validate;
 
-use think\Validate;
-
-class DataValidate extends Validate
+class DataValidate extends BaseValidate
 {
     protected $rule = [
-        'name' => 'require|max:10',
-        'email' => 'email',
-        'id' => 'number',
-        'class' => 'number'
+        'name' => 'require|max:25',
+        'age' => 'require|number|between:1,120',
+        'email' => 'require|email',
     ];
 
     protected $message = [
-        'name.require' => '姓名必填',
-        'name.max' => '姓名不能超过10个字符',
-        'email' => '邮件格式错误',
-        'id' => 'id应为数字',
-        'class' => 'class应为数字'
+        'name.require' => '名称必须填写',
+        'name.max' => '名称最多不能超过25个字符',
+        'age.number' => '年龄必须是数字',
+        'age.between' => '年龄只能在1-120之间',
+        'email' => '邮箱格式错误',
     ];
 
-
-    protected  $scene = [
-        'login' => ['name'],
-        'repass' => ['name','email'],
-        'false' => ['name','email','class']
+    protected $scene = [
+        'test' => ['name'],
+        'test1' => ['name','age']
     ];
+
+    protected function isPostiveInt($value, $rule, $data = '', $field)
+    {
+        if (is_numeric($value)) {
+            return true;
+        } else {
+            return $field . '必须是整数';
+        }
+    }
 }
